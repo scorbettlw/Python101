be251fec6a6e7acba908dbb2b0205feb92749b10
Sean Corbett <sean.corbett@lucidworks.com>
2019-03-21 21:24:39 -0600

    Added Lesson-001 folder

---
+++ Lesson-001_VariablesCrashCourse/about_me.py
@@ -0,0 +1,8 @@
+my_name = str(input("What is you name? "))
+
+my_age = int(input("What is your age? "))
+
+my_message = str(input("What would you like to say to the world? "))
+
+print("Hello, ",my_name, "you are ",my_age,"years old.")
+print("You said:",my_message)
---
+++ Lesson-001_VariablesCrashCourse/my_first_program.py
@@ -0,0 +1,25 @@
+my_name = "Brittany"
+
+print("Hello, my name is", my_name)
+print("Hello", my_name, ", welcome to learning Python!")
+
+my_name = "Sean"
+
+print("Actually, my name is", my_name,"!")
+print("Oh, sorry",my_name,"!")
+
+my_name = 22
+
+print("My name is not", my_name)
+
+risky_variable = str(my_name) + "Blah!"
+
+print("Will this work?",risky_variable)
+
+my_decimal = 0.5555555555555
+
+print("My decimal is",my_decimal)
+
+sum_total = my_decimal + my_name
+
+print("My total is:",sum_total)
